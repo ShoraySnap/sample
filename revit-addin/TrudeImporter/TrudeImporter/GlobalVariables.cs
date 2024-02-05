235dd11e657d5244221889d275a418b7790f8436
Fernando Arantes <fernando@snaptrude.com>
2024-02-05 11:03:10 -0300

    Add GlobalVariables to store UniqueId/ElementIds of walls with errors

--- revit-addin/TrudeImporter/TrudeImporter/GlobalVariables.cs
+++ revit-addin/TrudeImporter/TrudeImporter/GlobalVariables.cs
@@ -16,6 +16,9 @@ namespace TrudeImporter
 
         public static IDictionary<int, ElementId> LevelIdByNumber = new Dictionary<int, ElementId>();
         public static IDictionary<int, ElementId> childUniqueIdToWallElementId = new Dictionary<int, ElementId>();
+        public static IDictionary<int, ElementId> UniqueIdToElementId = new Dictionary<int, ElementId>();
+
+        public static List<ElementId> WallElementIdsToRecreate = new List<ElementId>();
 
         public static JArray materials;
         public static JArray multiMaterials;
@@ -25,10 +28,13 @@ namespace TrudeImporter
 
         public static void cleanGlobalVariables()
         {
+            Transaction = null;
             Document = null;
             RvtApp = null;
             LevelIdByNumber = new Dictionary<int, ElementId>();
             childUniqueIdToWallElementId = new Dictionary<int, ElementId>();
+            UniqueIdToElementId = new Dictionary<int, ElementId>();
+            WallElementIdsToRecreate = new List<ElementId>();
 
             materials = null;
             multiMaterials = null;
