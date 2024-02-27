c315dddb08197b35fb1c6d92b03addc2909d609a
Pooja <pooja@snaptrude.com>
2024-02-27 20:47:28 +0530

    s3 setup

---
+++ revit-addin/TrudeSerializer/Uploader/PreSignedURLResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrudeSerializer.Uploader
+{
+    internal class PreSignedURLResponse
+    {
+        public string url { get; set; }
+        public Dictionary<string, string> fields { get; set; }
+        
+    }
+
+}
