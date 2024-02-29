c66a3dc6b5972fbd195199721b16ca9398f45eae
Fernando Arantes <fernando@snaptrude.com>
2024-02-29 07:49:37 -0300

    Add CreateFamilyWithSymbol to new approach

--- revit-addin/TrudeImporter/TrudeImporter/Model/TrudeFurnitureNewApproach.cs
+++ revit-addin/TrudeImporter/TrudeImporter/Model/TrudeFurnitureNewApproach.cs
@@ -120,9 +120,83 @@ namespace TrudeImporter
                 System.Diagnostics.Debug.WriteLine("Furniture creation ERROR", e.ToString());
             }
         }
-        
-        private void CreateFurniture(FamilySymbol familySymbol, ElementId levelId, Wall wall, XYZ direction)
+        private void FlipFacing(FamilyInstance instance)
         {
+            bool facingFlipResult = instance.flipFacing();
+
+            if (!facingFlipResult)
+            {
+                XYZ normal = instance.FacingOrientation;
+                XYZ origin = (instance.Location as LocationPoint).Point;
+                Plane pl = Plane.CreateByNormalAndOrigin(normal, origin);
+                var ids = ElementTransformUtils.MirrorElements(GlobalVariables.Document, new List<ElementId>() { instance.Id }, pl, false);
+            }
+        }
+
+        public void SnaptrudeFlip(Element element, XYZ origin = null)
+        {
+            Parameter offset = GetOffsetParameter(element as FamilyInstance);
+
+            if (origin == null) origin = (element.Location as LocationPoint).Point;
+
+            //XYZ normal = new XYZ(0, 0, 1);
+            XYZ normal = new XYZ(0, 1, 0);
+
+            Plane pl = Plane.CreateByNormalAndOrigin(normal, origin);
+
+            double originalOffset = 0;
+            if (offset != null) originalOffset = offset.AsDouble();
+
+            var ids = ElementTransformUtils.MirrorElements(GlobalVariables.Document, new List<ElementId>() { element.Id }, pl, false);
+
+            if (offset != null) offset.Set(originalOffset);
+        }
+
+        public Parameter GetOffsetParameter(FamilyInstance instance)
+        {
+            if (instance == null) return null;
+
+            Parameter offset = GlobalVariables.RvtApp.VersionNumber == "2019"
+                ? instance.LookupParameter("Offset")
+                : instance.LookupParameter("Offset from Host");
+
+            return offset;
+        }
+
+        public void CreateFamilyWithSymbol(FamilySymbol familySymbol, Level level, double familyRotation, bool isFacingFlip, XYZ originOffset = null)
+        {
+            bool isSnaptrudeFlipped = Scaling.Z < 0;
+
+            FamilyInstance instance = GlobalVariables.Document.Create.NewFamilyInstance(XYZ.Zero, familySymbol, level, level, Autodesk.Revit.DB.Structure.StructuralType.UnknownFraming);
+
+            if (isFacingFlip && instance.FacingFlipped == false) FlipFacing(instance);
+
+            if (isSnaptrudeFlipped) SnaptrudeFlip(instance);
+
+            Transform offsetRotationTransform = Transform.CreateRotation(XYZ.BasisZ, familyRotation);
+
+            if (isSnaptrudeFlipped)
+                originOffset = offsetRotationTransform.OfPoint(originOffset);
+            else
+                originOffset = offsetRotationTransform.OfPoint(-originOffset);
+
+            Line rotationAxis = Line.CreateBound(originOffset, originOffset + XYZ.BasisZ);
+
+            instance.Location.Rotate(rotationAxis, -Rotation.Z);
+
+            if (isSnaptrudeFlipped)
+                instance.Location.Rotate(rotationAxis, -familyRotation);
+            else
+                instance.Location.Rotate(rotationAxis, familyRotation);
+
+            XYZ positionRelativeToLevel = new XYZ(
+                Position.X - originOffset.X,
+                Position.Y - originOffset.Y,
+                Position.Z - level.ProjectElevation + localBaseZ);
+
+            instance.Location.Move(positionRelativeToLevel);
+
+            element = instance;
         }
     }
 }
