249bf5fc1be60fa04531666b9fd576aec9e5a39c
Fernando Arantes <fernando@snaptrude.com>
2024-02-28 22:35:51 -0300

    Add FurnitureProperties

---
+++ revit-addin/TrudeImporter/TrudeImporter/Model/Properties/FurnitureProperties.cs
@@ -0,0 +1,76 @@
+﻿using Autodesk.Revit.DB;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+
+namespace TrudeImporter
+{
+    public class FurnitureProperties
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("storey")]
+        public int Storey { get; set; }
+
+        [JsonProperty("uniqueId")]
+        public int UniqueId { get; set; }
+
+        // Below is revitMetaData stuff
+
+        [JsonProperty("boundingBoxCenter")]
+        public XYZ BoundingBoxCenter { get; set; }
+
+        [JsonProperty("category")]
+        public string Category { get; set; }
+
+        [JsonProperty("existingElementId")]
+        public int? ExistingElementId { get; set; }
+
+        [JsonProperty("elements")]
+        public List<FurnitureInstanceProperties> Elements { get; set; }
+
+        [JsonProperty("facingFlipped")]
+        public bool? FacingFlipped { get; set; }
+
+        [JsonProperty("family")]
+        public string RevitFamilyName { get; set; }
+
+        [JsonProperty("familyRotation")]
+        public double? FamilyRotation { get; set; }
+
+        [JsonProperty("location")]
+        public XYZ Location { get; set; }
+
+        [JsonProperty("offset")]
+        public XYZ Offset { get; set; }
+
+        [JsonProperty("revitOffset")]
+        public double? RevitOffset { get; set; }
+
+        [JsonProperty("rotation")]
+        public double? Rotation { get; set; }
+
+        [JsonProperty("type")]
+        public string RevitFamilyType { get; set; }
+
+        [JsonProperty("sourceElementId")]
+        public int? SourceElementId { get; set; }
+
+    }
+
+    [Serializable]
+    public class FurnitureInstanceProperties
+    {
+        [JsonProperty("category")]
+        public string Category { get; set; }
+
+        [JsonProperty("family")]
+        public string RevitFamilyName { get; set; }
+
+        [JsonProperty("type")]
+        public string RevitFamilyType { get; set; }
+
+    }
+}
