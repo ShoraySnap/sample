c7d6e65d9a96ec5df15af6d2ee2ab2721ce169d3
Pooja <pooja@snaptrude.com>
2024-03-02 16:18:21 +0530

    Update default layer type for ceilings

--- revit-addin/TrudeSerializer/Types/TrudeCeilingType.cs
+++ revit-addin/TrudeSerializer/Types/TrudeCeilingType.cs
@@ -1,5 +1,6 @@
 using Autodesk.Revit.DB;
 using System.Collections.Generic;
+using System.Linq;
 using TrudeSerializer.Components;
 using TrudeSerializer.Utils;
 
@@ -8,6 +9,9 @@ namespace TrudeSerializer.Types
     internal class TrudeCeilingType
     {
         public List<TrudeLayer> layersData;
+
+        private static readonly double DEFAULT_LAYER_WIDTH = 25;
+        private static readonly string DEFAULT_LAYER_FUNCTION = "Structure";
 
         public TrudeCeilingType(List<TrudeLayer> layersData)
         {
@@ -20,21 +24,41 @@ namespace TrudeSerializer.Types
             Document document = GlobalVariables.Document;
             var elemType = document.GetElement(ceiling.GetTypeId()) as CeilingType;
             CompoundStructure compoundStructure = elemType?.GetCompoundStructure();
-            if (elemType == null || compoundStructure == null) return new TrudeCeilingType(layersData);
-            IList<CompoundStructureLayer> layers = compoundStructure.GetLayers();
-            foreach (CompoundStructureLayer layer in layers)
+            if (elemType == null || compoundStructure == null)
             {
-                double width = UnitConversion.ConvertToMillimeterForRevit2021AndAbove(layer.Width, UnitTypeId.Feet);
-                string function = layer.Function.ToString();
+                TrudeMaterial snaptrudeMaterial;
 
-                Material material = document.GetElement(layer.MaterialId) as Material;
+                ICollection<ElementId> materialIds = ceiling.GetMaterialIds(false);
+                if(materialIds.Count == 0)
+                {
+                    snaptrudeMaterial = TrudeMaterial.GetMaterial(null);
+                }
+                else
+                {
+                    snaptrudeMaterial = TrudeMaterial.GetMaterial(document.GetElement(materialIds.First()) as Material);
+                }
 
-                TrudeMaterial snaptrudeMaterial = TrudeMaterial.GetMaterial(material);
-
-                TrudeLayer Snaptrudelayer = new TrudeLayer(width, function, snaptrudeMaterial);
-
+                TrudeLayer Snaptrudelayer = new TrudeLayer(DEFAULT_LAYER_WIDTH, DEFAULT_LAYER_FUNCTION, snaptrudeMaterial);
                 layersData.Add(Snaptrudelayer);
             }
+            else
+            {
+                IList<CompoundStructureLayer> layers = compoundStructure.GetLayers();
+                foreach (CompoundStructureLayer layer in layers)
+                {
+                    double width = UnitConversion.ConvertToMillimeterForRevit2021AndAbove(layer.Width, UnitTypeId.Feet);
+                    string function = layer.Function.ToString();
+
+                    Material material = document.GetElement(layer.MaterialId) as Material;
+
+                    TrudeMaterial snaptrudeMaterial = TrudeMaterial.GetMaterial(material);
+
+                    TrudeLayer Snaptrudelayer = new TrudeLayer(width, function, snaptrudeMaterial);
+
+                    layersData.Add(Snaptrudelayer);
+                }
+            }
+            
             return new TrudeCeilingType(layersData);
         }
     }
