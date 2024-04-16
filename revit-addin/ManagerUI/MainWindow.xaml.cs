f5c74c8ec8d04d7a595830fd7d6610ebf71d493d
Gursehaj <gursehaj@snaptrude.com>
2024-04-17 00:18:25 +0530

    added new UI

---
+++ revit-addin/ManagerUI/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ManagerUI.Views;
+
+namespace ManagerUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow Instance;
+        public static ImageBrush BackgroundImage;
+        public static LoginView LoginView = new LoginView();
+        public static HomeView HomeView = new HomeView();
+        public static ExportView ExportView = new ExportView();
+        public MainWindow()
+        {
+            Instance = this;
+            InitializeComponent();
+            //BackgroundImage = new ImageBrush();
+            //BackgroundImage.ImageSource = new BitmapImage(new Uri("/Images/Snaptrude Manager.png", UriKind.Relative));
+            //this.Background = BackgroundImage;
+            // check for login logic here and set view accordingly
+            ActiveItem.Content = new LoginView();
+        }
+
+        private void closeManager(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ revit-addin/ManagerUI/Views/ExportView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ManagerUI.Views
+{
+    /// <summary>
+    /// Interaction logic for ExportView.xaml
+    /// </summary>
+    public partial class ExportView : UserControl
+    {
+        public event EventHandler OnSuccessfulExport;
+        public ExportView()
+        {
+            InitializeComponent();
+        }
+        //private void export(object sender, RoutedEventArgs e)
+        //{
+        //    OnSuccessfulExport?.Invoke(this, e);
+        //}
+        private void back(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Instance.ActiveItem.Content = MainWindow.HomeView;
+        }
+    }
+}
