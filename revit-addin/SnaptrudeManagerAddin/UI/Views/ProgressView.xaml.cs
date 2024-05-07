697dbedf6ac7b1fe1ee4d32705e03ab976c75b27
Fernando Arantes <fernando@snaptrude.com>
2024-05-06 22:12:11 -0300

    Set loading gif to progress view

--- revit-addin/SnaptrudeManagerAddin/UI/Views/ProgressView.xaml.cs
+++ revit-addin/SnaptrudeManagerAddin/UI/Views/ProgressView.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfAnimatedGif;
+
 
 namespace SnaptrudeManagerAddin.Views
 {
@@ -23,6 +25,7 @@ namespace SnaptrudeManagerAddin.Views
         public event EventHandler OnSuccessfulExport;
         public ProgressView()
         {
+            WpfAnimatedGif.ImageAnimationController a = null;
             InitializeComponent();
         }
         //private void export(object sender, RoutedEventArgs e)
