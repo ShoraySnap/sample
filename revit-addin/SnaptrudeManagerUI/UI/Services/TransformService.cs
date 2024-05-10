65c347bcaa6d50a38bfb5ff82b43e11b64d12f43
Taufiqul Alam <taufiqul@snaptrude.com>
2024-05-10 18:20:52 +0600

    Transform service

---
+++ revit-addin/SnaptrudeManagerUI/UI/Services/TransformService.cs
@@ -0,0 +1,27 @@
+﻿using SnaptrudeManagerUI.Stores;
+using SnaptrudeManagerUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnaptrudeManagerUI.UI.Services
+{
+    public class TransformService
+    {
+        private readonly NavigationStore navigationStore;
+        private readonly Func<ViewModelBase, ViewModelBase> transformViewModel;
+
+        public TransformService(NavigationStore navigationStore, Func<ViewModelBase, ViewModelBase> transformViewModel)
+        {
+            this.navigationStore = navigationStore;
+            this.transformViewModel = transformViewModel;
+        }
+
+        public void Transform()
+        {
+            navigationStore.CurrentViewModel = transformViewModel(navigationStore.CurrentViewModel);
+        }
+    }
+}
