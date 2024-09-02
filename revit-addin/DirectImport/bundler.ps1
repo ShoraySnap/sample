0c34cc00622c9c9f44f63ce6ac6dab101e74b83e
Shoray Singhal <shoryasinghall@gmail.com>
2024-09-02 15:07:38 +0530

    upadte to bundler

--- revit-addin/DirectImport/bundler.ps1
+++ revit-addin/DirectImport/bundler.ps1
@@ -6,6 +6,7 @@ param (
 $sourceDir = "./bin/Debug/Forge$RevitVersion"
 $bundleDir = "./assets/DirectImport.bundle/Contents"
 $zipPath = "./assets/Forge$RevitVersion/DirectImport.zip"
+# $forgeserverPath="C:\Users\Shroy\Documents\forgeserver\bundles"
 
 # Ensure the bundle directory exists
 New-Item -ItemType Directory -Force -Path $bundleDir
@@ -17,3 +18,8 @@ Copy-Item "$sourceDir/DirectImport.pdb" -Destination $bundleDir -Force
 # Compress the bundle
 New-Item -ItemType Directory -Force -Path "./assets/Forge$RevitVersion"
 Compress-Archive -Path "./assets/DirectImport.bundle" -DestinationPath $zipPath -Force
+ 
+# copy the file to $forgeserverPath
+# Copy-Item $zipPath -Destination $forgeserverPath -Force
+# Write-Host "Bundle copied to $forgeserverPath"
+
